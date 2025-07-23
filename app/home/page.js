970c745ff555248a88b25cd867c61c02ab2cce1f
Ashwini Upadhyay <ashwiniu@tech-radix.com>
2025-07-23 17:17:11 +0530

    first Commit

---
+++ app/home/page.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import Image from 'next/image'
+function page() {
+  return (
+    <Container className='h-100'>
+      <Row className='h-100'>
+        <Col md={6} className='d-flex justify-conetent-center align-items-center' >
+          <div className='lh-lg'>
+            <h6 className='text-uppercase'>
+              <span>W</span>
+              <span>e</span>
+              <span>l</span>
+              <span>c</span>
+              <span>o</span>
+              <span>m</span>
+              <span>e</span>&nbsp;
+              <span>t</span>
+              <span>o</span>&nbsp;
+              <span>m</span>
+              <span>y</span>&nbsp;
+              <span>w</span>
+              <span>o</span>
+              <span>r</span>
+              <span>l</span>
+              <span>d</span>
+            </h6>
+            <div>
+              <span className='fs-1'>Hi,</span>
+              <span>I'</span>
+              <span>m</span>&nbsp;
+              <span>A</span>
+              <span>s</span>
+              <span>h</span>
+              <span>w</span>
+              <span>i</span>
+              <span>n</span>
+              <span>i</span>&nbsp;
+              <span>U</span>
+              <span>p</span>
+              <span>a</span>
+              <span>d</span>
+              <span>h</span>
+              <span>y</span>
+              <span>a</span>
+              <span>y</span>
+            </div>
+            <h1 className='text-uppercase'>
+              <span>w</span>
+              <span>e</span>
+              <span>b</span>&nbsp;
+              <span>d</span>
+              <span>e</span>
+              <span>v</span>
+              <span>e</span>
+              <span>l</span>
+              <span>o</span>
+              <span>p</span>
+              <span>e</span>
+              <span>r</span>
+            </h1>
+            <p>
+              a passionate software developer specializing in React.js <br />and modern web development.
+            </p>
+            <button className='button outline me-1'>My Projects</button>
+            <button className='button ms-1' style={{backfround:'rgb(14, 14, 85)'}}>Download CV</button>
+          </div>
+        </Col>
+        <Col md={6} className='d-flex justify-conetent-center align-items-center' >
+          <Image src='/demonSlayer-Photoroom.png' width={500} height={500} alt='..' placeholder='blur' blurDataURL='data ...' />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const card = useRef();
+  const heading = useRef();
+  const con = useRef();
+  const navigate = useRouter();
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(con.current, {
+      duration: 0.5,
+      opacity: 0,
+    })
+    .from(heading.current.children, {
+      duration: 0.3,
+      y: 10,
+      opacity: 0,
+      stagger: 0.1,
+      ease: "power"
+    }, "-=0.2")
+    .from(card.current.children, {
+      duration: 1,
+      y: 50,
+      opacity: 0,
+      stagger: 0.2,
+      ease: "bounce"
+    }, "-=0.2");
+  }, []);
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Container ref={con} className='home d-flex flex-column h-100 justify-content-center align-items-center'>
+      <h4 ref={heading} className="mb-5" style={{ overflowY: 'clip' }}>
+        <span>C</span>
+        <span>h</span>
+        <span>o</span>
+        <span>o</span>
+        <span>s</span>
+        <span>e</span>&nbsp;
+        <span>P</span>
+        <span>r</span>
+        <span>e</span>
+        <span>f</span>
+        <span>e</span>
+        <span>r</span>
+        <span>e</span>
+        <span>d</span>&nbsp;
+        <span>T</span>
+        <span>h</span>
+        <span>e</span>
+        <span>a</span>
+        <span>m</span>
+        <span>e</span>
+      </h4>
+      <Row ref={card}>
+        <Col sm={4}><Card onClick={() =>navigate.push('/home')}>Soft Theame</Card></Col>
+        <Col sm={4}><Card>Mild Theame</Card></Col>
+        <Col sm={4}><Card>Hard Theame</Card></Col>
+      </Row>
+    </Container>
   );
 }
