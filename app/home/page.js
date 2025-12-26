1ca57e1282e1992af61818c8cc650852efe1f807
Ashwini Upadhyay <ashwiniu@tech-radix.com>
2025-12-26 16:03:47 +0530

    modified the page.

--- app/home/page.js
+++ app/home/page.js
@@ -1,88 +1,60 @@
-import React from 'react'
-import { Button, Col, Container, Row } from 'react-bootstrap'
-import Image from 'next/image'
-function page() {
-  return (
-    <Container className='h-100'>
-      <Row className='h-100'>
-        <Col
-          md={6}
-          className="d-flex justify-content-center align-items-center position-relative"
-        >
-          <div className='lh-lg'>
-            <h6 className='text-uppercase'>
-              <span>W</span>
-              <span>e</span>
-              <span>l</span>
-              <span>c</span>
-              <span>o</span>
-              <span>m</span>
-              <span>e</span>&nbsp;
-              <span>t</span>
-              <span>o</span>&nbsp;
-              <span>m</span>
-              <span>y</span>&nbsp;
-              <span>w</span>
-              <span>o</span>
-              <span>r</span>
-              <span>l</span>
-              <span>d</span>
-            </h6>
-            <div>
-              <span className='fs-1 me-1'>Hi,</span>
-              <span>I</span>
-              <span>&apos;</span>
-              <span>m</span>&nbsp;
-              <span>A</span>
-              <span>s</span>
-              <span>h</span>
-              <span>w</span>
-              <span>i</span>
-              <span>n</span>
-              <span>i</span>&nbsp;
-              <span>U</span>
-              <span>p</span>
-              <span>a</span>
-              <span>d</span>
-              <span>h</span>
-              <span>y</span>
-              <span>a</span>
-              <span>y</span>
-            </div>
-            <h1 className='text-uppercase'>
-              <span>w</span>
-              <span>e</span>
-              <span>b</span>&nbsp;
-              <span>d</span>
-              <span>e</span>
-              <span>v</span>
-              <span>e</span>
-              <span>l</span>
-              <span>o</span>
-              <span>p</span>
-              <span>e</span>
-              <span>r</span>
-            </h1>
-            <p>
-              a passionate software developer specializing in React.js <br />and modern web development.
-            </p>
-            <button className='button outline me-1'>My Projects</button>
-            <a href='/Ashwini_Upadhyay_Resume_1-10-25.pdf' download target="_blank" rel="noopener noreferrer"><button className='button ms-1' style={{ backfround: 'rgb(14, 14, 85)' }}>Download CV</button></a>
-          </div>
-        </Col>
-        <Col md={6} className='d-flex justify-conetent-center align-items-center' >
-          <Image
-            src="/demonSlayer-Photoroom.png"
-            width={700}
-            height={700}
-            alt=".."
-            className="img-fluid DS"
-            priority
-          />
-        </Col>
-      </Row>
-    </Container>
-  )
+
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+
+const AnimatedText = ({ text }) => {
+    return (
+        <>
+            {text.split('').map((char, index) => (
+                <span key={index} className="animated-letter">{char === ' ' ? '\u00A0' : char}</span>
+            ))}
+        </>
+    );
+};
+
+function HomePage() {
+    return (
+        <Container className="h-100 home">
+            <Row className="h-100 align-items-center">
+                <Col md={6} className="d-flex justify-content-center align-items-center">
+                    <div className="text-center text-md-start lh-lg">
+                        <h6 className="text-uppercase">
+                            <AnimatedText text="Welcome to my world" />
+                        </h6>
+                        <div className="my-4">
+                            <span className="fs-1 me-2">Hi, I&apos;m</span>
+                            <span className="fs-1 text-uppercase">
+                                <AnimatedText text="Ashwini Upadhyay" />
+                            </span>
+                        </div>
+                        <h1 className="text-uppercase">
+                            <AnimatedText text="web developer" />
+                        </h1>
+                        <p className="lead my-4">
+                            A passionate software developer specializing in React.js and modern web development.
+                        </p>
+                        <div className="d-flex justify-content-center justify-content-md-start">
+                            <button className="button outline me-3">My Projects</button>
+                            <a href="/Ashwini_Upadhyay_Resume_1-10-25.pdf" download target="_blank" rel="noopener noreferrer">
+                                <button className="button">Download CV</button>
+                            </a>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={6} className="d-none d-md-flex justify-content-center align-items-center">
+                    <Image
+                        src="/demonSlayer-Photoroom.png"
+                        width={600}
+                        height={600}
+                        alt="Demon Slayer"
+                        className="img-fluid DS"
+                        priority
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-export default page
+export default HomePage;
